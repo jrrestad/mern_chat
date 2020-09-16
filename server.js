b58d520af296fe4b9edc2605a06f880bd58cf502
jtclayt <jt.clayton92@yahoo.com>
2020-09-16 13:17:31 -0700

    Add server file changes

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-const ExampleRoutes = require('./server/routes/example.routes');
+const SocketIo = require('socket.io');
 
 const app = express();
-const PORT = 8000;
+const PORT = 8001;
 
 require('./server/config/mongoose.config');
 app.use(cors());
 app.use(express.json(), express.urlencoded({extended: true}));
-ExampleRoutes(app);
+// ExampleRoutes(app);
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+const io = SocketIo(server);
+io.on("connection", socket => {
+  console.log(socket.id);
+  socket.emit("welcome", "Hello there new person");
+});
